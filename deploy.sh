4c0b0991feaaf560d36b4bb3d980b1923abf70e8
Tianyi Wang <tywang666@gmail.com>
2018-06-11 22:12:00 +0800

    ssl deploy

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,8 @@
 # export LC_CTYPE="en_US.UTF-8"
 # dpkg-reconfigure locales
 # python -m pip install "django<2" django-sslserver baidu-aip
+# scp -i ~/Downloads/kp-nj7tubox app.key app.pem root@139.198.188.74:~
 
 zip -r nlp_chat_bot.zip nlp_chat_bot static manage.py
 scp -i ~/Downloads/kp-nj7tubox nlp_chat_bot.zip root@139.198.188.74:~
-ssh -i ~/Downloads/kp-nj7tubox root@139.198.188.74 "kill $(cat nlp_chat_bot/run.pid); rm -rf nlp_chat_bot; unzip nlp_chat_bot.zip -d nlp_chat_bot; cd nlp_chat_bot; nohup python manage.py runsslserver 0.0.0.0:443 > /dev/null 2>&1 & echo $! > run.pid"
+ssh -i ~/Downloads/kp-nj7tubox root@139.198.188.74 "kill $(cat nlp_chat_bot/run.pid); rm -rf nlp_chat_bot; unzip nlp_chat_bot.zip -d nlp_chat_bot; cd nlp_chat_bot; nohup python manage.py runsslserver 0.0.0.0:443 --certificate ../app.pem --key ../app.key > /dev/null 2>&1 & echo $! > run.pid"
