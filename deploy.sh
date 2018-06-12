08e0026f52727dea1cfb1c85b080a45a2097b938
Tianyi Wang <tywang666@gmail.com>
2018-06-13 01:01:25 +0800

    update deploy script

--- deploy.sh
+++ deploy.sh
@@ -8,4 +8,4 @@
 
 zip -r nlp_chat_bot.zip nlp_chat_bot static manage.py
 scp -i ~/Downloads/kp-nj7tubox nlp_chat_bot.zip root@139.198.188.74:~
-ssh -i ~/Downloads/kp-nj7tubox root@139.198.188.74 "kill $(cat nlp_chat_bot/run.pid); rm -rf nlp_chat_bot; unzip nlp_chat_bot.zip -d nlp_chat_bot; cd nlp_chat_bot; nohup python manage.py runsslserver 0.0.0.0:443 --certificate ../app.pem --key ../app.key > /dev/null 2>&1 & echo $! > run.pid"
+ssh -i ~/Downloads/kp-nj7tubox root@139.198.188.74 "kill `ps auxw | grep runsslserver | awk  '{print $2}'`; rm -rf nlp_chat_bot; unzip nlp_chat_bot.zip -d nlp_chat_bot; cd nlp_chat_bot; nohup python manage.py runsslserver 0.0.0.0:443 --certificate ../app.pem --key ../app.key &"
